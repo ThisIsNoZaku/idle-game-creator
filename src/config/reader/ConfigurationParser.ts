481d5572757201219e1519e9420fa054707d1705
damien <damienmarble@gmail.com>
2018-09-11 22:29:56 -0400

    Removed unused import.

--- src/config/reader/ConfigurationParser.ts
+++ src/config/reader/ConfigurationParser.ts
@@ -1,5 +1,4 @@
 import Model, {LetsMakeAGame} from "../Model";
-import {AssertionError} from "assert";
 
 function generateInvalidConfigurationError(message:string){
     throw new Error(`Invalid Configuration File - ${message}`);
