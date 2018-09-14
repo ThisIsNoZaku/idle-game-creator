b5056316d7cc9e560c54075346f8e5b36d0cb955
damien <damienmarble@gmail.com>
2018-09-14 18:38:32 -0400

    Corrected call to dispatch after configuration retrieval.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore } from "redux";
-import { Provider } from "react-redux";
+import {createStore} from "redux";
+import {Provider} from "react-redux";
 import RootReducer from "./state/reducers/RootReducer";
 import YamlConfigurationParser from "./config/reader/YamlConfigurationParser";
 import axios from "axios";
@@ -14,33 +14,33 @@ let queryParameters = new URLSearchParams(window.location.search);
 
 let initialState = {};
 
-if(queryParameters.has("config")){
+if (queryParameters.has("config")) {
     let configLocation = queryParameters.get("config");
     console.info(`config query parameter defined (${configLocation}), attempting to retrieve.`);
-    axios.get(`${queryParameters.get("config")}`).then(response=>{
-        console.info("Config query retrieved");
+    axios.get(`${queryParameters.get("config")}`).then(response => {
+        console.info("Config query retrieved, dispatching configuration.");
         store.dispatch({
             type: "POPULATE_CONFIG",
             config: new YamlConfigurationParser().parse(response.data)
-        }, error=>{
-            console.error(error);
-        }).catch(error=>{
-            console.error(error);
-            store.dispatch({
-                type: "POPULATE_CONFIG_ERROR",
-                error
-            });
+        })
+    }, error => {
+        console.error(error);
+    }).catch(error => {
+        console.error(error);
+        store.dispatch({
+            type: "POPULATE_CONFIG_ERROR",
+            error
         });
     });
 } else {
-    initialState.error = "Location of config file is missing. Add a query parameter like\n"+
+    initialState.error = "Location of config file is missing. Add a query parameter like\n" +
         "?config=http://somewebsite.com/path/to/file.yaml";
 }
 
-const store = createStore(RootReducer, initialState);
+const store = createStore(RootReducer, initialState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <App/>
     </Provider>, document.getElementById('root'));
 registerServiceWorker();
