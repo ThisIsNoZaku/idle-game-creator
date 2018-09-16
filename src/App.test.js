5c1444b72de7caadfa7c360e63e58ca769930bb7
damien <damienmarble@gmail.com>
2018-09-16 00:13:50 -0400

    Fixed App test.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore } from "redux";
+let store = createStore();
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Provier store={store}><App /></Provier>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
