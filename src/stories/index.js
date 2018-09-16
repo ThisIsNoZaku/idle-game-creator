a3e3972c8ae4e294fdb03b092db463f245485458
damien <damienmarble@gmail.com>
2018-09-16 03:38:47 -0400

    Updated stories.

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,52 +1,62 @@
 import React from 'react';
 
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+import {storiesOf} from '@storybook/react';
+import {action} from '@storybook/addon-actions';
+import {linkTo} from '@storybook/addon-links';
 
-import { App } from "../App";
+import {App} from "../App";
 
-import { ButtonComponent } from "../components/Button";
+import {ButtonComponent} from "../components/Button";
 import {LayoutSection} from "../components/LayoutSection";
+import {GameRenderer} from "../components/GameRenderer";
+import SectionConfiguration from "../config/model/layout/SectionConfiguration";
+import {ButtonConfiguration} from "../config/model/ButtonConfiguration";
+import GameConfiguration from "../config/model/GameConfiguration";
 
 storiesOf("App", module)
-    .add("can render an error message", () => <App error="Some Error Message" />);
+    .add("can render an error message", () => <App error="Some Error Message"/>)
+    .add("", () => <App/>);
+
+storiesOf("GameRenderer", module)
+    .add("can render a game related error", () => <GameRenderer error="Some Error Message"/>)
+    .add("can render a layout based on a config", () => <GameRenderer config={{
+        layout: {
+            "buttons": new SectionConfiguration("buttons", "Buttons", [])
+        }
+    }}/>)
 
 storiesOf('Button', module)
-  .add('with text', () => <ButtonComponent text="Hello World" onClick={action("Hello World!")}>Hello Button</ButtonComponent>);
+    .add('with text', () => <ButtonComponent text="Hello World" onClick={action("Hello World!")}>Hello
+        Button</ButtonComponent>);
 
 storiesOf("LayoutSection", module)
-    .add("can contain buttons", ()=> <LayoutSection
-        header="Buttons"
-        children={[
-        React.createElement(ButtonComponent, {buttonKey:"one", text:"One"}),
-        React.createElement(ButtonComponent, {buttonKey:"two", text:"Two"}),
-        React.createElement(ButtonComponent, {buttonKey:"three", text:"Three"})]
-    } ></LayoutSection>)
-    .add("can have a text Header", ()=><LayoutSection header="Text Header" ></LayoutSection>)
-    .add("can layout contents horizontally", ()=> <LayoutSection
-        header="Horizontal Buttons"
-        direction="horizontal"
-        children={[
-        React.createElement(ButtonComponent, {buttonKey:"one", text:"One"}),
-        React.createElement(ButtonComponent, {buttonKey:"two", text:"Two"}),
-        React.createElement(ButtonComponent, {buttonKey:"three", text:"Three"})]
-    } ></LayoutSection>)
-    .add("can contain other LayoutSections", ()=> <LayoutSection
-        header="Top Level"
-        direction="horizontal"
-        children={[
-            <LayoutSection
-                header="Left Nested"
-                direction="horizontal" children={[
-                    <ButtonComponent buttonKey="leftOne" text="Left One"/>,
-                    <ButtonComponent buttonKey="leftTwo" text="Left Two"/>
-                ]}/>,
-            <LayoutSection
-                header="Right Nested"
-                children={[
-                    <ButtonComponent buttonKey="rightOne" text="Right One"/>,
-                    <ButtonComponent buttonKey="rightTwo" text="Right Two"/>
-                ]}/>
-        ]}/>
-        )
+    .add("can contain buttons", () => <LayoutSection
+        identifier="buttons"
+        config={new GameConfiguration(null, {
+            "buttonOne" : new ButtonConfiguration("buttonOne", "Button One"),
+            "buttonTwo" : new ButtonConfiguration("buttonTwo", "Button Two"),
+            "buttonThree" : new ButtonConfiguration("buttonThree", "Button Three")
+        }, {
+            "buttons" : new SectionConfiguration("buttons", null, ["buttonOne", "buttonTwo", "buttonThree"])
+        })}
+    ></LayoutSection>)
+    .add("can have a text Header", () => <LayoutSection
+        identifier="buttons"
+        config={new GameConfiguration(null, {
+            "buttonOne" : new ButtonConfiguration("buttonOne", "Button One"),
+            "buttonTwo" : new ButtonConfiguration("buttonTwo", "Button Two"),
+            "buttonThree" : new ButtonConfiguration("buttonThree", "Button Three")
+        }, {
+            "buttons" : new SectionConfiguration("buttons", "Buttons")
+        })}
+    ></LayoutSection>)
+    .add("can layout contents horizontally", () => <LayoutSection
+        identifier="buttons"
+        config={new GameConfiguration(null, {
+            "buttonOne" : new ButtonConfiguration("buttonOne", "Button One"),
+            "buttonTwo" : new ButtonConfiguration("buttonTwo", "Button Two"),
+            "buttonThree" : new ButtonConfiguration("buttonThree", "Button Three")
+        }, {
+            "buttons" : new SectionConfiguration("buttons", null, ["buttonOne", "buttonTwo", "buttonThree"], "horizontal")
+        })}
+    ></LayoutSection>);
