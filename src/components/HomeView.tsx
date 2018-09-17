83f0427fca0abe58c92154868f5c4a550e05a074
damien <damienmarble@gmail.com>
2018-09-17 03:32:42 -0400

    Created a "Home View" index page.

--- src/components/HomeView.tsx
+++ src/components/HomeView.tsx
@@ -1,7 +1,14 @@
 import * as React from "react";
+import Grid from "@material-ui/core/Grid/Grid";
 
 export default class HomeView extends React.Component {
-    render () {
-        return(<div></div>)
+    render() {
+        return (<Grid
+            container>
+            <Grid item>
+                Idle Game Creator is an engine for creating your own Idle/Clicker games. Inspired by the <a
+                href="http://orteil.dashnet.org/igm/">Idle Game Maker</a>
+            </Grid>
+        </Grid>)
     }
 }
