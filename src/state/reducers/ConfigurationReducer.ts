bcdacc9c09bb77ea3120080e49ac8ad0d5b84de0
Damien Marble <damienmarble@gmail.com>
2018-10-06 17:46:20 +0000

    Modified ConfigurationReducer to throw on violation instead of console.assert.

--- src/state/reducers/ConfigurationReducer.ts
+++ src/state/reducers/ConfigurationReducer.ts
@@ -5,17 +5,17 @@ import {Action} from "redux";
 import PopulateConfigAction from "../actions/PopulateConfigAction";
 
 export default function(state: any, action: Action<any>) {
-    console.info("ConfigurationReducer called");
     if (state === undefined) {
         return null;
     }
 
     if (action.type === "POPULATE_CONFIG") {
         const populateAction = (action as PopulateConfigAction);
-        console.assert(!_.isEmpty(populateAction.config), "POPULATE_CONFIG called but config was empty.");
+        if (_.isEmpty(populateAction.config)) {
+            throw new Error("POPULATE_CONFIG called but config was empty.");
+        }
         return (populateAction.config as GameConfiguration);
-    } else {
-        console.info(`ConfigurationReducer ignoring action of type ${action.type}`);
     }
+    
     return state;
 }
