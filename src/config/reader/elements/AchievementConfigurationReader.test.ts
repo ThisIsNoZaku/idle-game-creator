f2d23dd2aa1c2c508cd464109c36080e5301d0b7
Damien Marble <damienmarble@gmail.com>
2019-01-05 18:34:36 +0000

    Added AchievementConfiguration.

---
+++ src/config/reader/elements/AchievementConfigurationReader.test.ts
@@ -0,0 +1,30 @@
+import AchievementConfigurationReader from "./AchievementConfigurationReader";
+
+describe("Achievement configuration reader", () => {
+    const reader = AchievementConfigurationReader.instance();
+    it("transforms objects into AchievementConfiguration objects", () => {
+        const input = {
+            name: "upgrade",
+            description: "description",
+            requirements: {
+                resources: {
+                    resource: "1 total",
+                },
+            },
+        };
+        const out = reader.read("key", input);
+        
+        expect(out.key).toBe("key");
+        expect(out.name).toBe("upgrade");
+        expect(out.description).toBe("description");
+        expect(out.requirements).toEqual({
+            resources: {
+                resource: {
+                    current: 0,
+                    lifetimeMax: 0,
+                    lifetimeTotal: 1,
+                },
+            },
+        });
+    });
+});
---
+++ src/config/reader/elements/AchievementConfigurationReader.ts
@@ -0,0 +1,16 @@
+import ItemConfigurationReader from "./ItemConfigurationReader";
+import AchievementConfiguration from "../../model/AchievementConfiguration";
+import RequirementsTranslator from "../RequirementsTranslator";
+
+export default class AchievementConfigurationReader implements ItemConfigurationReader<AchievementConfiguration> {
+    private static _instance: AchievementConfigurationReader = new AchievementConfigurationReader();
+    public static instance() {
+        return this._instance;
+    }
+    
+    public read(key:string, input: any) {
+        return new AchievementConfiguration(key, input.name, input.description,
+            RequirementsTranslator(input.requirements)
+        );
+    }
+}
